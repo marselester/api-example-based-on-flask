295e00efe22bb6f6acad24da90fa09a678bfe4c2
marselester <marselester@ya.ru>
2014-06-17 20:16:20 +0700

    Add support of Flask-Login in api v3

--- messaging_api_v3/__init__.py
+++ messaging_api_v3/__init__.py
@@ -8,6 +8,7 @@ API version 3.
 """
 from api_utils import ResponsiveFlask
 from api_utils.auth import Hawk
+from messaging import login_manager
 
 app = ResponsiveFlask(__name__, instance_relative_config=True)
 app.config.from_object('config.base')
@@ -15,5 +16,6 @@ app.config.from_object('config.dev')
 app.config.from_pyfile('config.py')
 
 hawk = Hawk(app)
+login_manager.init_app(app)
 
 from . import views
