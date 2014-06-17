e870ba60ab8d9821c070b5f11d026a4d2952d6ae
marselester <marselester@ya.ru>
2014-06-17 17:53:02 +0700

    Add config structure

---
+++ instance/config.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+# This file should not be under VCS control.
+# It was added intentionally as an example.
+SECRET_KEY = 'some secret key'
--- messaging_api_v3/__init__.py
+++ messaging_api_v3/__init__.py
@@ -9,7 +9,10 @@ API version 3.
 from api_utils import ResponsiveFlask
 from api_utils.auth import Hawk
 
-app = ResponsiveFlask(__name__)
+app = ResponsiveFlask(__name__, instance_relative_config=True)
+app.config.from_object('config.base')
+app.config.from_object('config.dev')
+app.config.from_pyfile('config.py')
 
 hawk = Hawk(app)
 
